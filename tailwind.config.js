280c92dc0ee21bfe7def1e3e1d6d6edc46716c35
Aviralbatra-gep <aviral_batra@gep.com>
2022-07-06 03:51:55 +0530

    improved ui

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/home.html","./templates/form.html","./templates/result.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
